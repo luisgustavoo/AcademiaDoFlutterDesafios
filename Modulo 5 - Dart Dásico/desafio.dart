d7c1ca3127720adfb5064fd319705d25457554cf
luisgustavoo <luisgustavovieirasantos@gmail.com>
2020-10-19 15:40:27 -0300

    Ajustes

--- Modulo 5 - Dart Dásico/desafio.dart
+++ Modulo 5 - Dart Dásico/desafio.dart
@@ -10,27 +10,16 @@ void main() {
     'João Rahman|55|Jornalista|SP',
   ];
 
-  pacientes.retainWhere((p) => int.parse(p.split('|')[1]) > 20);
-
   // ** Apresente a quantidade de pacientes com mais de 20 anos
   print('Apresente a quantidade de pacientes com mais de 20 anos');
-  print('Existem ${pacientes.length} com mais de 20 anos');
+  print(
+      'Existem ${pacientes.where((p) => int.parse(p.split('|')[1]) > 20).length} com mais de 20 anos');
 
   print('========================================');
 
   // ** Agrupar os pacientes por familia(considerar o sobrenome) apresentar por familia.
   print(
       'Agrupar os pacientes por familia(considerar o sobrenome) apresentar por familia.');
-  pacientes = [
-    'Rodrigo Rahman|35|desenvolvedor|SP',
-    'Manoel Silva|12|estudante|MG',
-    'Joaquim Rahman|18|estudante|SP',
-    'Fernando Verne|35|estudante|MG',
-    'Gustavo Silva|40|estudante|MG',
-    'Sandra Silva|40|estudante|MG',
-    'Regina Verne|35|estudante|MG',
-    'João Rahman|55|Jornalista|SP',
-  ];
 
   List<Paciente> listaDePacientes = [];
   for (var paciente in pacientes) {
